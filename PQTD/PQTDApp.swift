7edbd1cd41c65838599cebfbfad7538487e99c01
William Tang <wjt40398@gmail.com>
2023-01-14 15:21:05 -0500

    Initial Commit

---
+++ PQTD/PQTDApp.swift
@@ -0,0 +1,17 @@
+//
+//  PQTDApp.swift
+//  PQTD
+//
+//  Created by William Tang on 2023-01-14.
+//
+
+import SwiftUI
+
+@main
+struct PQTDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
